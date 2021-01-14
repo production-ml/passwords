b795daee68cab5fc5a8701d913c90f6484e97bdd
aguschin <1aguschin@gmail.com>
2021-01-14 15:14:59 +0300

    Squashed 'packages/app/' changes from 051b6cd..a1b4d98
    a1b4d98 running pre-commit hooks against all files
    7a415af update pre-commit hooks and add instruction to readme
    2a125d9 add pre-commit hooks
    
    git-subtree-dir: packages/app
    git-subtree-split: a1b4d98793a84e8755d0090a7da8f8413f975d42

--- train.py
+++ train.py
@@ -3,10 +3,12 @@ from lstm_model.base_model import PasswordLSTM
 
 def main():
     password_model = PasswordLSTM()
-    password_model.fit(train_file='train.csv.zip', test_file='Xtest.csv.zip', epochs=1, batch_size=512)
-    password_model.save_tokenizer('tokenizer_2.pickle')
-    password_model.save_model('one_epoch_model')
+    password_model.fit(
+        train_file="train.csv.zip", test_file="Xtest.csv.zip", epochs=1, batch_size=512
+    )
+    password_model.save_tokenizer("tokenizer_2.pickle")
+    password_model.save_model("one_epoch_model")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
