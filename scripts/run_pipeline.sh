85f4b1a0c42e453d23fd02f6a292187d7eba0954
aguschin <1aguschin@gmail.com>
2021-04-27 15:27:28 +0300

    scripts update

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-echo "Get data from kaggle"
-kaggle competitions download -c dmia-sport-2019-fall-intro -f train.csv -p data/raw/
-kaggle competitions download -c dmia-sport-2019-fall-intro -f Xtest.csv -p data/raw/
+echo "Download data"
+sh download_data.sh
 echo "Running ML training pipeline"
 PYTHONPATH=./package/ python scripts/train.py
 echo "Listing the resulting files and folders"
