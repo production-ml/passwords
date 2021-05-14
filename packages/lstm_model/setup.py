6b1b68097dac228d33f5bb62a7e260ca920f2f1b
aguschin <1aguschin@gmail.com>
2021-05-14 19:58:55 +0300

    creating lstm package and fixing Pipfle.lock

---
+++ packages/lstm_model/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lstm_model",
+    packages=find_packages(),
+    version="0.1.0",
+    description="LSTM model to predict password complexity",
+    author="Vitaly Belov",
+)
