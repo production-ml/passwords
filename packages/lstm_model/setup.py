d6afe72acf3561d4ee964d22df28cf0709e4858b
aguschin <1aguschin@gmail.com>
2021-05-17 12:56:29 +0300

    update setup.py with install_requires

--- packages/lstm_model/setup.py
+++ packages/lstm_model/setup.py
@@ -3,6 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name="lstm_model",
     packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "sklearn",
+        "keras",
+        "tensorflow-cpu",
+        "loguru",
+    ],
     version="0.1.0",
     description="LSTM model to predict password complexity",
     author="Vitaly Belov",
